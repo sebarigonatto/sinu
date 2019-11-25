93acc702d4ac8d84359f3f579a9de99b8570a52c
SIEC CSIN Velasquez Emanuel <evelasquez@armada.mil.ar>
2019-11-25 10:24:16 -0300

    Correccion errores controlador Home.

--- SINU/Controllers/HomeController.cs
+++ SINU/Controllers/HomeController.cs
@@ -6,12 +6,14 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using SINU.Models;
+using SINU.ViewModels;
 
 namespace SINU.Controllers
 {
     public class HomeController : Controller
-
     {
+        SINUEntities db= new SINUEntities();
         public ActionResult Index()
         {
             return View(db.vPeriodosInscrip.ToList());
