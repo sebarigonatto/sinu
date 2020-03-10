944fd067a0a2e71c4ec3ed7130e3d620bdcb08f5
jmorales <jmorales@ara.mil.ar>
2020-03-10 13:04:45 -0300

    Codigo que pasa los datos de la base de seguridad a las tablas de AspNetUser, AspNetRoles,AspNetUserRoles

--- SINU/Startup.cs
+++ SINU/Startup.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using SINU.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 [assembly: OwinStartupAttribute(typeof(SINU.Startup))]
 namespace SINU
@@ -9,6 +14,47 @@ namespace SINU
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRoles();
+        }
+        private void createRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            
+            SINUEntities db = new SINUEntities();
+            
+            List<vSeguridad_Grupos> perfiles = db.vSeguridad_Grupos.Where(m => m.codAplicacion == "SINU").ToList();
+
+            foreach (vSeguridad_Grupos item in perfiles)
+            {
+                //Crea el Rol
+                if (!roleManager.RoleExists(item.codGrupo))
+                {
+                    var role = new IdentityRole();
+                    role.Name = item.codGrupo;
+                    roleManager.Create(role);
+
+                }
+            }
+
+
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            List<vSeguridad_Grupos_Usuarios> UsuarioYPerfiles = db.vSeguridad_Grupos_Usuarios.Where(m => m.codAplicacion == "SINU").ToList();
+            ApplicationUser user;
+            //IdentityRole role;
+
+            foreach (var item in UsuarioYPerfiles)
+            {
+                     user = UserManager.FindByName(item.codUsuario);
+               if (!UserManager.IsInRole(user.Id,item.codGrupo))
+                {
+                    //role = roleManager.FindByName(item.codGrupo);
+                    var result1 = UserManager.AddToRole(user.Id, item.codGrupo);
+
+                }
+            }
+
         }
     }
+    
 }
