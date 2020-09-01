895368f68899574686ea3f7e4b08619fa61e969b
cayampi <cayampi@SIECWI60>
2020-09-01 09:10:27 -0300

    Se actualizo el modelo de la base de datos(Se agrego la vista VDataProblema)

--- SINU/Controllers/DelegacionController.cs
+++ SINU/Controllers/DelegacionController.cs
@@ -283,8 +283,17 @@ namespace SINU.Controllers
         [HttpGet]
         public ActionResult Documentacion(int id)
         {
+            vInscripcionDetalle vdetalle;
+            vdetalle = db.vInscripcionDetalle.FirstOrDefault(m => m.IdPersona == id);
+            var NyA = vdetalle.Nombres +" "+ vdetalle.Apellido;
+
+
             IDPersonaVM personaVM = new IDPersonaVM();
             personaVM.ID_PER = id;
+            personaVM.NomyApe = NyA;
+
+
+
             return View(personaVM);
         }
         [HttpPost]
@@ -402,22 +411,20 @@ namespace SINU.Controllers
         [HttpGet]
         public ActionResult DataProblema(int id, int id_per)
         {
-            //vInscripcionDetalle vdetalle;
-            //vdetalle = db.vInscripcionDetalle.FirstOrDefault(m => m.IdPersona==id_per);
 
             ProblemaEcontradoVM problema = new ProblemaEcontradoVM();
             problema.ID_PER = id_per;
             problema.IdDataverificacion = id;
             problema.Comentario = "";
-            //IDPersonaVM personaVM = new IDPersonaVM();
 
-
-            //DataVerificacion data;
-            //data = db.DataVerificacion.FirstOrDefault(m => m.IdDataVerificacion == id);
-            //ViewBag.Nombre = vdetalle.Nombres;
-            //ViewBag.Apellido = vdetalle.Apellido;
-            //ViewBag.IdInscripcion = vdetalle.IdInscripcion;
-            //ViewBag.Texto = data.Descripcion;
+            vInscripcionDetalle vdetalle;
+            vdetalle = db.vInscripcionDetalle.FirstOrDefault(m => m.IdPersona == id_per);
+            DataVerificacion data;
+            data = db.DataVerificacion.FirstOrDefault(m => m.IdDataVerificacion == id);
+            ViewBag.Nombre = vdetalle.Nombres;
+            ViewBag.Apellido = vdetalle.Apellido;
+            ViewBag.IdInscripcion = vdetalle.IdInscripcion;
+            ViewBag.Texto = data.Descripcion;
 
             return View(problema);
         }
